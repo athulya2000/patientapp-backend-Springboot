faa09b8ac2e7ba479d393996ea2992629587f68f
athulya2000 <athulyav00@gmail.com>
2022-12-06 22:23:04 +0530

    initial version

---
+++ src/main/java/com/example/patientapp_backend/controller/PatientController.java
@@ -0,0 +1,12 @@
+package com.example.patientapp_backend.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PatientController {
+    @PostMapping
+    public String PatientAdd(){
+        return "Welcome to patient add page";
+    }
+}
