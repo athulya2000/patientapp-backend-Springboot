5460f1655b7a59c83dc6d58cbea9a2513ba92b28
athulya2000 <athulyav00@gmail.com>
2022-12-08 14:48:00 +0530

    delete

--- src/main/java/com/example/patientapp_backend/dao/PatientDao.java
+++ src/main/java/com/example/patientapp_backend/dao/PatientDao.java
@@ -1,6 +1,8 @@
 package com.example.patientapp_backend.dao;
 
 import com.example.patientapp_backend.model.Patients;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +13,8 @@ public interface PatientDao extends CrudRepository<Patients,Integer> {
 
     @Query(value="SELECT `id`, `contactno`, `dateofappointment`, `doctorname`, `patientid`, `patientname` FROM `patients` WHERE `patientid`=:patientid",nativeQuery = true)
     List<Patients> Searchpatients(@Param("patientid") String patientid);
+     @Modifying
+     @Transactional
+    @Query(value="DELETE FROM `patients` WHERE `id`= :id",nativeQuery = true)
+    void deletePatient(@Param("id") Integer id);
 }
